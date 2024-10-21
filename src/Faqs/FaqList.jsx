68ffeba1a26edb7037d160ab2470a1398851d83d
balaji <BalajiReactjsRepository>
2024-10-21 10:29:47 +0530

    faqs

--- src/Faqs/FaqList.jsx
+++ src/Faqs/FaqList.jsx
@@ -1,19 +1,19 @@
-import React, { useRef, useEffect, useCallback, useState } from 'react';
-import { downloadExcel } from 'react-export-table-to-excel';
-import Cookies from 'js-cookie';
-import axios from 'axios';
+import React, { useRef, useEffect, useCallback, useState } from "react";
+import { downloadExcel } from "react-export-table-to-excel";
+import Cookies from "js-cookie";
+import axios from "axios";
 
-import { MdDelete, MdModeEditOutline } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { MdDelete, MdModeEditOutline } from "react-icons/md";
+import { Link } from "react-router-dom";
 
-import swalHandle from '../Pages/ErrorHandler';
-import AdminSideBar from '../Pages/AdminSideBar';
+import swalHandle from "../Pages/ErrorHandler";
+import AdminSideBar from "../Pages/AdminSideBar";
 
 const FaqList = () => {
   const [totalFaqs, setTotalFaqs] = useState(0);
   const [fullFaqsList, setFullFaqsList] = useState([]); // Store full list
   const [filteredFaqsList, setFilteredFaqsList] = useState([]); // For pagination
-  const [faqType, setFaqType] = useState('');
+  const [faqType, setFaqType] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const tableRef = useRef(null);
 
@@ -47,8 +47,11 @@ const FaqList = () => {
       pageNumbers.push(
         <li
           key={i}
-          className={`page-item ${i === currentPage ? 'active' : ''}`}>
-          <span className='page-link' onClick={() => handlePageChange(i)}>
+          className={`page-item ${
+            i === currentPage ? "active activePage" : ""
+          }`}
+        >
+          <span className="page-link" onClick={() => handlePageChange(i)}>
             {i}
           </span>
         </li>
@@ -58,12 +61,12 @@ const FaqList = () => {
   };
 
   const faqTypes = [
-    'General',
-    'Order',
-    'Tracking',
-    'Payment',
-    'Return',
-    'Product',
+    "General",
+    "Order",
+    "Tracking",
+    "Payment",
+    "Return",
+    "Product",
   ];
 
   const baseUrl = `${process.env.REACT_APP_API_URL}/faqs`;
@@ -118,14 +121,14 @@ const FaqList = () => {
 
   const truncateText = (text, maxLength) => {
     if (text.length <= maxLength) return text;
-    return text.substring(0, maxLength) + '...';
+    return text.substring(0, maxLength) + "...";
   };
 
-  const header = ['FAQ Question', 'FAQ Answer', 'FAQ Type'];
+  const header = ["FAQ Question", "FAQ Answer", "FAQ Type"];
   const handleDownloadExcel = () => {
     downloadExcel({
-      fileName: 'faqs',
-      sheet: 'faqs-list',
+      fileName: "faqs",
+      sheet: "faqs-list",
       tablePayload: {
         header,
         body: fullFaqsList.map((faq) => ({
@@ -138,18 +141,22 @@ const FaqList = () => {
   };
 
   return (
-    <div className='adminSec'>
+    <div className="adminSec">
       <div>
         <AdminSideBar />
       </div>
-      <div className='commonSec'>
-        <div className='col-12 mt-2 mb-2 d-flex justify-content-between'>
+      <div className="commonSec">
+        <div className="col-12 mt-2 mb-2 d-flex justify-content-between align-items-center">
           <h4>FAQ's</h4>
-          <button disabled={!fullFaqsList.length} onClick={handleDownloadExcel}>
-            Export to Excel
+          <button
+            className="btn btn-success"
+            disabled={!fullFaqsList.length}
+            onClick={handleDownloadExcel}
+          >
+            Export
           </button>
           <select value={faqType} onChange={changeFaqType}>
-            <option value=''>All</option>
+            <option value="">All</option>
             {faqTypes.map((faq, index) => (
               <option key={index} value={faq}>
                 {faq}
@@ -157,19 +164,19 @@ const FaqList = () => {
             ))}
           </select>
 
-          <Link to='/faqs/create'>Create FAQ</Link>
+          <Link to="/faqs/create">Create FAQ</Link>
         </div>
-        <div className='col-sm-12'>
+        <div className="col-sm-12">
           {filteredFaqsList.length ? (
             <>
-              <table className='table' ref={tableRef}>
+              <table className="table" ref={tableRef}>
                 <thead>
                   <tr>
-                    <th scope='col'>S.No</th>
-                    <th scope='col'>FAQ Question</th>
-                    <th scope='col'>FAQ Answer</th>
-                    <th scope='col'>FAQ Type</th>
-                    <th scope='col'>Actions</th>
+                    <th scope="col">S.No</th>
+                    <th scope="col">FAQ Question</th>
+                    <th scope="col">FAQ Answer</th>
+                    <th scope="col">FAQ Type</th>
+                    <th scope="col">Actions</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -181,57 +188,64 @@ const FaqList = () => {
                       <td>{faq.azst_faq_type}</td>
                       <td>
                         <MdDelete
-                          className='icons'
+                          className="icons"
                           onClick={() => deleteFaq(faq.azst_faq_id)}
-                        />{' '}
+                        />{" "}
                         <Link to={`/edit-faq/${faq.azst_faq_id}`}>
-                          <MdModeEditOutline className='icons' />
+                          <MdModeEditOutline className="icons" />
                         </Link>
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
-              <p>
-                Showing {Math.min(currentPage * logsPerPage, totalFaqs)} out of{' '}
-                {totalFaqs}
-              </p>
             </>
           ) : (
             <p>No FAQs Found</p>
           )}
         </div>
-        {totalPages > 1 && (
-          <div className='mt-2 d-flex justify-content-end'>
-            <nav aria-label='Page navigation'>
-              <ul className='pagination'>
-                <li
-                  className={`page-item ${
-                    currentPage === 1 ? 'disabled' : ''
-                  }`}>
-                  <span
-                    className='page-link'
-                    aria-label='Previous'
-                    onClick={() => handlePageChange(currentPage - 1)}>
-                    &laquo;
-                  </span>
-                </li>
-                {renderPageNumbers()}
-                <li
-                  className={`page-item ${
-                    currentPage === totalPages ? 'disabled' : ''
-                  }`}>
-                  <span
-                    className='page-link'
-                    aria-label='Next'
-                    onClick={() => handlePageChange(currentPage + 1)}>
-                    &raquo;
-                  </span>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        )}
+        <div className="col-sm-12 d-flex justify-content-between align-items-center">
+          <p>
+            Showing {Math.min(currentPage * logsPerPage, totalFaqs)} out of{" "}
+            {totalFaqs}
+          </p>
+
+          {totalPages > 1 && (
+            <div className="mt-2 d-flex justify-content-end">
+              <nav aria-label="Page navigation">
+                <ul className="pagination">
+                  <li
+                    className={`page-item ${
+                      currentPage === 1 ? "disabled" : ""
+                    }`}
+                  >
+                    <span
+                      className="page-link"
+                      aria-label="Previous"
+                      onClick={() => handlePageChange(currentPage - 1)}
+                    >
+                      &laquo;
+                    </span>
+                  </li>
+                  {renderPageNumbers()}
+                  <li
+                    className={`page-item ${
+                      currentPage === totalPages ? "disabled" : ""
+                    }`}
+                  >
+                    <span
+                      className="page-link"
+                      aria-label="Next"
+                      onClick={() => handlePageChange(currentPage + 1)}
+                    >
+                      &raquo;
+                    </span>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
