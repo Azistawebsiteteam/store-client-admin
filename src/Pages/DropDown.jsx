0d2d141068ac6875a46e585fbdcff6f633138f1d
balaji <BalajiReactjsRepository>
2024-10-24 10:11:42 +0530

    navbar fun changed

--- src/Pages/DropDown.jsx
+++ src/Pages/DropDown.jsx
@@ -1,6 +1,13 @@
-import Cookies from 'js-cookie';
-import { Link } from 'react-router-dom';
-import './Admin.css';
+import Cookies from "js-cookie";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { RiArrowDropDownFill, RiArrowDropUpFill } from "react-icons/ri";
+import { RiAccountPinCircleLine } from "react-icons/ri";
+import { CiLogout } from "react-icons/ci";
+import { MdLockReset } from "react-icons/md";
+import "./Admin.css";
+
+// import { IoMdArrowDropdown } from "react-icons/io";
 
 function BasicButtonExample() {
   const [showDropdown, setShowDropdown] = useState(false);
@@ -11,34 +18,69 @@ function BasicButtonExample() {
   };
 
   const handleUserLogout = () => {
-    window.location.replace('/adminLoginPage');
+    window.location.replace("/adminLoginPage");
     Cookies.remove(adminToken);
   };
   return (
-    <div className='dropdown-center'>
-      <button
-        className='btn btn-secondary dropdown-toggle'
-        type='button'
-        data-bs-toggle='dropdown'
-        aria-expanded='false'>
-        User
-      </button>
-      <ul className='dropdown-menu customDropDown'>
-        <li>
-          <Link className='dropdown-item' to='/manageAccount'>
-            Manage Accounts
-          </Link>
-        </li>
-
-        <li>
-          <a
-            className='dropdown-item'
-            onClick={handleUserLogout}
-            href='/adminLoginPage'>
-            Logout
-          </a>
-        </li>
-      </ul>
+    <div className="dropdown">
+      {showDropdown ? (
+        <RiArrowDropUpFill
+          size={26}
+          style={{ color: "#fff" }}
+          onClick={toggleDropdown}
+        />
+      ) : (
+        <RiArrowDropDownFill
+          size={26}
+          style={{ color: "#fff" }}
+          onClick={toggleDropdown}
+        />
+      )}
+      {showDropdown && (
+        <ul className="dropdownMenu">
+          <li>
+            <Link
+              className="dropdown-item dropdownItems mb-1"
+              to="/manageAccount"
+              style={{ fontSize: "14px", fontWeight: "500" }}
+              onClick={() => setShowDropdown(false)}
+            >
+              <RiAccountPinCircleLine
+                size={20}
+                className="me-1"
+                style={{ fontSize: "14px", fontWeight: "500" }}
+              />
+              Manage Accounts
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="dropdown-item dropdownItems mb-1"
+              to="/reset-password"
+              style={{ fontSize: "14px", fontWeight: "500" }}
+              onClick={() => setShowDropdown(false)}
+            >
+              <MdLockReset
+                size={20}
+                className="me-1"
+                style={{ fontSize: "14px", fontWeight: "500" }}
+              />
+              Reset Password
+            </Link>
+          </li>
+          <li>
+            <a
+              className="dropdown-item dropdownItems"
+              href="/adminLoginPage"
+              onClick={handleUserLogout}
+              style={{ fontSize: "14px", fontWeight: "500" }}
+            >
+              <CiLogout className="me-1" size={20} />
+              Log out
+            </a>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
